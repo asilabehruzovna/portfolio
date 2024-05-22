15f9f30533d7505a7de03bc03e9fa634a8b18328
asilabehruzovna <asilabehruzovna@gmail.com>
2024-05-22 16:17:58 +0500

    port

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Gif from "../assets/dev1.gif"
+import ReactImg from "../assets/react.png"
+import ReduxImg from "../assets/redux.png"
+import Sass from "../assets/sass.png"
+import Social from './Social'
+
+//animation
+import { Fade ,Flip,Zoom} from 'react-reveal';
+
+const Header = () => {
+  return (
+    <div className='px-3 lg:px-10 pt-[110px] lg:pt-0 bg-[#EDF2F8] w-full lg:h-[100vh] flex lg:flex-row flex-col items-center justify-center gap-10' id='home'>
+        {/* Download CV  */}
+        <Flip right>
+        <div className='flex flex-col gap-6 md:gap-10 items-center md:items-end w-full max-w-[400px]'>
+            <div className='flex justify-center items-center bg-[white] gap-3 md:gap-10 w-full max-w-[350px] p-1 lg:p-3  rounded-[15px] shadow-lg'>
+                <p className='text-[35px] lg:text-[40px]'>👋</p>
+                <p className='flex flex-col'>
+                    <span className='text-slate-400 text-[18px] lg:text-[20px]'>Hello, I am</span>
+                    <span className='font-bold text-[25px] lg:text-[30px]'>Asila</span>
+                </p>
+            </div>
+            <div className='w-full bg-[white] max-w-[300px] p-3 text-slate-400 text-[14px] lg:text-[18px] text-center  rounded-[15px] shadow-lg'>FRONTEND/REACT DEVELOPER</div>
+            <a href='rezume.pdf' download type="button" class="text-white bg-[#313bac] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-4 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Downlaod CV</a>
+        </div>
+        </Flip>
+        {/* gif for developer  */}
+        <Zoom up duration={1000} delay={200}>
+        <div className='text-center'>
+            <img src={Gif} alt="Gif" />
+        </div>
+        </Zoom>
+        <Fade right>
+        <div className='flex lg:flex-col gap-10 py-5'>
+            <div className='w-[75px] h-[75px] lg:w-[150px] lg:h-[150px] rounded-full bg-[white] shadow-2xl flex justify-center items-center'>
+                <img src={ReduxImg} className='w-[80%]' alt="img" />
+            </div>
+            <div className='w-[100px] h-[100px] lg:w-[200px] lg:h-[200px] rounded-full bg-[white] shadow-2xl flex justify-center items-center'>
+            <img src={ReactImg} className='w-[90%]' alt="img" />
+
+            </div>
+            <div className='w-[50px] h-[50px] lg:w-[100px] lg:h-[100px] rounded-full bg-[white] shadow-2xl flex justify-center items-center'>
+            <img src={Sass} className='w-[80%]' alt="img" />
+            </div>
+        </div>
+        </Fade>
+
+        <Social/>
+    </div>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Providers } from './redux/provider';
+// import App from './App';
 
+const App = lazy(()=>import('./App'))
+const Loader = lazy(()=>import('./components/Loader'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Suspense fallback={<Loader/>}>
+  <Providers>
     <App />
+  </Providers>
+  </Suspense>
   </React.StrictMode>
 );
+
 
 
